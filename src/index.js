6acc03bd1192bf3fd7f99447cf762557fce50ae5
Siddharth Sinha <siddharthsinha9752@gmail.com>
2020-07-21 15:32:59 +0530

    now really added Styletron

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider as StyletronProvider, DebugEngine } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
+
+const debug =
+  process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
+
+// 1. Create a client engine instance
+const engine = new Styletron();
+
 
 ReactDOM.render(
   <React.StrictMode>
